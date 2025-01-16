d7148321401ae0e3fe142a2d99cd66fdab776426
Obosob <obosob@riseup.net>
2025-01-16 16:50:29 +0000

    Port AKB Raine to Vial

---
+++ keyboards/akb/raine/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x84, 0x7F, 0xC2, 0x55, 0x6D, 0xDD, 0xD2, 0xB7}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
